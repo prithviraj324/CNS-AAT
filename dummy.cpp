bcaffd82e3dc76d23d6c00a5018da224c3f18d1b
prithviraj324 <pritvirajc.cs19@bmsce.ac.in>
2022-06-19 02:57:15 +0530

    basic workings added, modify/delete later

---
+++ dummy.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main() {
+    string s;
+    //  \u200B -> zero width space
+    //  \u200C -> zero width non-joiner
+    //  \u2060 -> zero width joiner
+    //  \uFEFF -> zero width nbsp
+    // demo website: https://www.diffchecker.in/
+    string c=u8"\u2060";
+    s+=c;
+    /*cout<<"size:"<<s.size()<<endl;
+    cout<<"char c1:"<<c<<"-----"<<endl;
+    cout<<"string:"<<s;
+    cout<<"printing\u200Cthis \n\n\n";*/
+    cout<<"input text";
+    return 0;
+}
---
+++ read.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main() {
+    string s;
+    cin>>s;
+    cout<<"read the string:"<<s<<"\n\n\n";
+    return 0;
+}
