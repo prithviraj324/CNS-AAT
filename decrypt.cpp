393b7e0849262e6db6b00532dff57fdf4991a92c
prithviraj324 <pritvirajc.cs19@bmsce.ac.in>
2023-05-15 18:19:23 +0530

    push

--- decrypt.cpp
+++ decrypt.cpp
@@ -2,8 +2,30 @@
 #include<iostream>
 using namespace std;
 int main() {
+    cout<<"In decryption:"<<endl;
     string s;
-    cin>>s; //read from output(stdout) of decrypt file
-    cout<<"read the string:"<<s<<"\n\n\n";
+    cin>>s; //read from output(stdout) of encrypt file
+    int sz=s.size();
+    int st=0, ans =0;
+    string res;
+    while(st<=sz-2) {
+        //cout<<"st:"<<st<<", end:"<<st+3<<endl;
+
+        string tmp=s.substr(st,st+3);
+        wstring_convert<std::codecvt_utf8_utf16<wchar_t>> convert;
+        wstring tmp8 = convert.from_bytes( tmp );
+        if(tmp==u8"\u2060"){
+            ans+=1;
+            ans = ans >> 1;
+        }
+        
+        else if(tmp==u8"\u200C"){
+            ans >> 1;
+        }
+        if(ans>10)
+        res.push_back((char)ans);
+        st+=3;
+    }
+    cout<<"Hidden String is:" + s<<endl;
     return 0;
 }
--- encrypt.cpp
+++ encrypt.cpp
@@ -8,12 +8,48 @@ int main() {
     //  \u2060 -> zero width joiner
     //  \uFEFF -> zero width nbsp
     // demo website: https://www.diffchecker.in/
-    string c=u8"\u2060";
-    s+=c;
-    cout<<"size:"<<s.size()<<endl;
-    cout<<"char c1:"<<c<<"-----"<<endl;
-    cout<<"string:"<<s;
-    cout<<"printing\u200Cthis \n\n\n";
-    cout<<"input text";
+
+    //  \u2060->1
+    //  \u200C->0
+    cout<<"In encryption:"<<endl;
+    string one=u8"\u2060";
+    string zero=u8"\u200C";
+
+    //input
+    string plain,hide;
+    cout<<"Enter plain text: ";
+    cin>>plain; 
+    cin.clear();
+    cin.ignore(INT_MAX, '\n');
+    cout<<"Enter text to hide: ";
+    cin>>hide;
+    cout<<endl;
+
+    //encryption
+    string enc;
+    for(char c:hide) {
+        int ch=(int)c;
+        //cout<<"char ascii:"<<ch<<endl;
+        for(int i=0;i<8;i++) {
+            //cout<<ch<<",";
+            if(ch%2 == 0) {
+                enc.insert(0,zero);
+                ch /= 2;
+            }
+            else if(ch%2 == 1){
+                enc.insert(0,one);
+                ch /= 2;
+            }
+        }
+    }
+    //cout<<endl<<enc.size()<<endl;
+    int sz=enc.size();
+    int st=0;
+    int id=3;
+    st = plain.size();
+    //cout<<"Encrypted msg: "<<enc<<endl;
+    string tmp = plain.substr(id+1, plain.size());
+    plain = plain.substr(0,id+2) + enc + tmp;
+    cout<<"Hidden msg is:"<<hide<<endl;
     return 0;
 }
